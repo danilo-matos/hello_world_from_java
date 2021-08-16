5d86f026178590022a1dbd9a3d1c6960383eed3f
Danilo <danilo03@live.com.pt>
2021-08-16 10:15:47 -0300

    Secobd Up date

--- src/app/App.java
+++ src/app/App.java
@@ -5,6 +5,24 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("It´s me again");
 		System.out.println("Hello World");
+		
+		// Variables
+		
+		int aValue; // int for inteiro
+		int age=20;
+		short day;
+		long money = 69854213;
+		double cm = 1.78;
+		
+		aValue = 40;
+		day = 365;
+		
+		System.out.println(aValue);
+		System.out.println(age );
+		System.out.println(day);
+		System.out.println(money);
+		System.out.println(cm);
+		
 	}
 
 }
